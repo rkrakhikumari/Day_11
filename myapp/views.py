b7b1fc7dfce345a160075aae91123acf4d3d46f6
rkrakhikumari <rkrakhikumari012@gmail.com>
2023-11-05 22:15:11 +0530

    day 11

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-
+from datetime import datetime
+from myapp.models import Contact
 # Create your views here.
 def index(request):
     return render(request, "index.html")
@@ -14,5 +15,15 @@ def services(request):
     return render(request, "services.html")
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name') 
+        email = request.POST.get('email') 
+        phone = request.POST.get('phone') 
+        desc = request.POST.get('desc') 
+        contact = Contact(name=name, email = email, phone = phone, desc= desc, date= datetime.today())
+        contact.save()
+        
+
+
     return render(request, "contact.html")
 
