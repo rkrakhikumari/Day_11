253c3985a15ee2db6ad1fd7babcc784bee7d17d6
rkrakhikumari <rkrakhikumari012@gmail.com>
2023-11-05 15:22:25 +0530

    day 11

---
+++ myapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def home(request):
+    return HttpResponse("this is home page")
+
+def about(request):
+    return HttpResponse("this is about page")
+
+def services(request):
+    return HttpResponse("this is service page")
+
+def contact(request):
+    return HttpResponse("this is contact page")
+
